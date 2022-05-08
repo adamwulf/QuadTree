e6bfdc1a4680ec67731d1c322c691d6dd9e52889
Adam Wulf <adam.wulf@gmail.com>
2022-05-08 01:46:38 -0500

    updating test to show how fast it can be

--- Sources/QuadTree/QuadTree.swift
+++ Sources/QuadTree/QuadTree.swift
@@ -9,7 +9,7 @@ import Foundation
 import CloudKit
 import SwiftToolbox
 
-public protocol Locatable: Equatable {
+public protocol Locatable: Hashable {
     func intersects(_ rect: CGRect) -> Bool
 }
 
@@ -21,62 +21,92 @@ public struct QuadTree<Element: Locatable> {
 
     private var data: QuadType
     let frame: CGRect
+    var origin: CGPoint {
+        return frame.origin
+    }
+    var size: CGSize {
+        return frame.size
+    }
+    let MaxDepth: Int = 10
     let maxPerLeaf: Int
+    private let level: Int
     var depth: Int {
         switch self.data {
         case .leaf:
-            return 1
+            return level
         case .branch(let tl, let tr, let bl, let br):
-            return 1 + max(tl.depth, tr.depth, bl.depth, br.depth)
+            return max(tl.depth, tr.depth, bl.depth, br.depth)
+        }
+    }
+    var items: Set<Element> {
+        switch self.data {
+        case .leaf(let items):
+            return Set(items)
+        case .branch(let tl, let tr, let bl, let br):
+            return tl.items.union(tr.items).union(bl.items).union(br.items)
         }
     }
 
     var count: Int {
-        switch self.data {
-        case .leaf(let items):
-            return items.count
-        case .branch(let tl, let tr, let bl, let br):
-            return tl.count + tr.count + bl.count + br.count
-        }
+        return items.count
     }
 
     public init(origin: CGPoint = .zero, size: CGSize, maxPerLeaf: Int = 10) {
         self.maxPerLeaf = maxPerLeaf
         frame = CGRect(origin: origin, size: size)
         data = .leaf([])
+        level = 1
     }
 
-    init(frame: CGRect, data: QuadType, maxPerLeaf: Int) {
+    init(frame: CGRect, data: QuadType, maxPerLeaf: Int, level: Int) {
         self.frame = frame
         self.data = data
         self.maxPerLeaf = maxPerLeaf
+        self.level = level
     }
+
+    // MARK: - Public
 
     public func inserting(_ element: Element) -> QuadTree {
         guard element.intersects(frame) else { return self }
         switch self.data {
         case .leaf(let items):
-            return QuadTree(frame: frame, data: .leaf(items + [element]), maxPerLeaf: maxPerLeaf)
+            guard
+                items.count >= maxPerLeaf,
+                level < MaxDepth
+            else {
+                let updated = items.contains(element) ? items : items + [element]
+                return QuadTree(frame: frame, data: .leaf(updated), maxPerLeaf: maxPerLeaf, level: level)
+            }
+            let tlFr = CGRect(origin: origin, size: size / 2)
+            let trFr = CGRect(origin: origin + CGVector(dx: size.width, dy: 0), size: size / 2)
+            let blFr = CGRect(origin: origin + CGVector(dx: 0, dy: size.height), size: size / 2)
+            let brFr = CGRect(origin: origin + size / 2, size: size / 2)
+            let tl = QuadTree(frame: tlFr, data: .leaf([]), maxPerLeaf: maxPerLeaf, level: level + 1)
+            let tr = QuadTree(frame: trFr, data: .leaf([]), maxPerLeaf: maxPerLeaf, level: level + 1)
+            let bl = QuadTree(frame: blFr, data: .leaf([]), maxPerLeaf: maxPerLeaf, level: level + 1)
+            let br = QuadTree(frame: brFr, data: .leaf([]), maxPerLeaf: maxPerLeaf, level: level + 1)
+            var tree =  QuadTree(frame: frame,
+                                 data: .branch(tl, tr, bl, br),
+                                 maxPerLeaf: maxPerLeaf,
+                                 level: level)
+            for item in items + [element] {
+                tree.insert(item)
+            }
+            return tree
         case .branch(let tl, let tr, let bl, let br):
             return QuadTree(frame: frame,
                             data: .branch(tl.inserting(element),
                                           tr.inserting(element),
                                           bl.inserting(element),
                                           br.inserting(element)),
-                            maxPerLeaf: maxPerLeaf)
+                            maxPerLeaf: maxPerLeaf,
+                            level: level)
         }
     }
 
     mutating public func insert(_ element: Element) {
         guard element.intersects(frame) else { return }
-        switch self.data {
-        case .leaf(let items):
-            data = .leaf(items + [element])
-        case .branch(let tl, let tr, let bl, let br):
-            data = .branch(tl.inserting(element),
-                           tr.inserting(element),
-                           bl.inserting(element),
-                           br.inserting(element))
-        }
+        self = inserting(element)
     }
 }
--- Tests/QuadTreeTests/QuadTreeTests.swift
+++ Tests/QuadTreeTests/QuadTreeTests.swift
@@ -9,7 +9,14 @@ import XCTest
 @testable import QuadTree
 import SwiftToolbox
 
-extension CGRect: Locatable { }
+extension CGRect: Locatable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(origin.x)
+        hasher.combine(origin.y)
+        hasher.combine(size.width)
+        hasher.combine(size.height)
+    }
+}
 
 final class QuadTreeTests: XCTestCase {
     func testInsert() throws {
@@ -30,14 +37,28 @@ final class QuadTreeTests: XCTestCase {
         XCTAssertEqual(quadtree.frame, CGRect(x: 0, y: 0, width: 100, height: 100))
     }
 
-    func testInsertMax() throws {
+    func testPreventDuplicates() throws {
         var quadtree: QuadTree<CGRect> = QuadTree(size: CGSize(100, 100))
 
         for _ in 0..<20 {
             quadtree.insert(CGRect(x: 10, y: 10, width: 10, height: 10))
         }
 
-        XCTAssertEqual(quadtree.count, 20)
+        XCTAssertEqual(quadtree.count, 1)
+        XCTAssertEqual(quadtree.depth, 1)
+    }
+
+    func testMultipleLevels() throws {
+        var quadtree: QuadTree<CGRect> = QuadTree(size: CGSize(1000, 1000))
+
+        for _ in 0..<200 {
+            quadtree.insert(CGRect(x: CGFloat.random(in: -90..<990),
+                                   y: CGFloat.random(in: -90..<990),
+                                   width: CGFloat.random(in: 0..<100),
+                                   height: CGFloat.random(in: 0..<100)))
+        }
+
+        XCTAssertGreaterThan(quadtree.count, 50)
         XCTAssertGreaterThan(quadtree.depth, 1)
     }
 }
